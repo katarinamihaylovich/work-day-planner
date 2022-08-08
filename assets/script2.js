cce9a0bb706e73ead2bb9632297da03221c96178
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-08-08 14:47:42 -0700

    major updates to js and html

--- assets/script2.js
+++ assets/script2.js
@@ -1,9 +1,49 @@
 var date = moment().format("dddd, MMMM Do");
 $("#currentDay").text(date);
 
-var userInputEl = $("#input");
-var userInput = userInputEl.val().trim();
 
-function printUserInfo(){
-    var planning = $("")
+//functino to create update time, loop through time-blocks, specify current hour
+
+function hourColor(){
+    var currentHour = moment().hours();
+
+    $(".time-block").each(function(){
+        var calendarHour = parseInt($(this).attr("id").split('-')[1]);
+    
+        if (calendarHour < currentHour){
+            $(this).addClass("past");
+        }
+        else if (calendarHour === currentHour){
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future")
+        }
+    });
 }
+
+hourColor();
+
+//Key is the name, value is the input/items
+// 
+
+$(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, value);
+});
+
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
